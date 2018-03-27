acdf721847e9f1d2ab04956962db009ea7d2d4e7
Kevin <kevincrisanto@gmail.com>
2018-03-26 17:08:54 -0700

    Next level loads when user destroys all bricks

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,23 +3,29 @@ using System.Collections;
 
 public class Brick : MonoBehaviour {
 	
+	public static int breakableCount = 0;
 	public Sprite[] hitSprites;
 	
 	private int timesHit;
 	private LevelManager levelManager;
+	private bool isBreakable; 
+	
 	// Use this for initialization
 	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		if (isBreakable){
+			breakableCount++;
+		}
 		timesHit = 0;
 		levelManager = GameObject.FindObjectOfType<LevelManager>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 	
 	void OnCollisionEnter2D (Collision2D col){
-		bool isBreakable = this.tag == "Breakable";
 		if(isBreakable){
 			HandleHits();
 		}
@@ -29,6 +35,8 @@ public class Brick : MonoBehaviour {
 		timesHit++;
 		int maxHits = hitSprites.Length + 1;
 		if (timesHit >= maxHits){
+			breakableCount--;
+			levelManager.BrickDestroyed();
 			Destroy (gameObject);
 		} else{
 			LoadSprites();
